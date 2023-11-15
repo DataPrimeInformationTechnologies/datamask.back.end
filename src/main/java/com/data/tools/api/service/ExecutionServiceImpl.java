ad13152860f0cc8967374a52439b88297598d8b4
Cihad Alan <chdln@x1ns-Mac.local>
2023-11-15 15:52:41 +0300

    execution sql completed

--- src/main/java/com/data/tools/api/service/ExecutionServiceImpl.java
+++ src/main/java/com/data/tools/api/service/ExecutionServiceImpl.java
@@ -2,43 +2,42 @@ package com.data.tools.api.service;
 
 import java.util.List;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.provider.HibernateUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ExecutionServiceImpl implements ExecutionService {
 
-	@Autowired
-	protected SessionFactory sessionFactory;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
 
 	@Override
+	@Transactional
 	public List<Object> runSQLCommand() {
 
-		Session currentSession =  getCurrentSession();
-		Transaction tx = currentSession.beginTransaction();
-		// currentSession.query("select emp_id, emp_name, emp_salary from Employee");
-		// currentSession.createNativeQuery(
-        //     "select * from dados_usuario where fk_usuario = :id_usuario","");
-		// // Get All Employees
-		
-		// SQLQuery query = session.createSQLQuery();
-		// List<Object[]> rows = query.list();
-		// for (Object[] row : rows) {
-		// 	Employee emp = new Employee();
-		// 	emp.setId(Long.parseLong(row[0].toString()));
-		// 	emp.setName(row[1].toString());
-		// 	emp.setSalary(Double.parseDouble(row[2].toString()));
-		// 	System.out.println(emp);
-		// }
+		String sql = """
+CREATE TABLE accounts (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP
+);
+""";
+		Query query = entityManager.createNativeQuery(sql);
+		System.out.println(query.executeUpdate());
 		return null;
 	}
 
-	protected Session getCurrentSession() {
-        return sessionFactory.getCurrentSession();
-    }
+
 
 }
