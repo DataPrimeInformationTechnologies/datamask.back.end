73dbfefffdce452c1c17c58e2262a69e6a550f90
Berat Akdoğan <83817984+aberatakdogan@users.noreply.github.com>
2023-12-07 21:19:57 +0300

    Update DBConfigurationRepository.java

--- project/src/main/java/com/springpostgre/project/config/DBConfigurationRepository.java
+++ project/src/main/java/com/springpostgre/project/config/DBConfigurationRepository.java
@@ -2,5 +2,9 @@ package com.springpostgre.project.config;
 
 import com.springpostgre.project.config.DBConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface DBConfigurationRepository extends JpaRepository<DBConfiguration, Long>{
 }
+
