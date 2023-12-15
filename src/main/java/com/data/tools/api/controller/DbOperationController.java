f33ce8feff3f0b503277aab3e45fac12d336acfc
Eren Eroğul <eren.eroul1903@gmail.com>
2023-12-15 16:49:35 +0300

    DbOperationController added.

---
+++ src/main/java/com/data/tools/api/controller/DbOperationController.java
@@ -0,0 +1,40 @@
+package com.data.tools.api.controller;
+
+import com.data.tools.api.service.DbOperationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/db-op")
+public class DbOperationController {
+
+    public final DbOperationService dbOperationService;
+    @GetMapping("/getSchema/{dbConfigId}")
+    public ResponseEntity<List<String>> getSchema(@PathVariable Long dbConfigId) throws SQLException {
+        List<String> result = dbOperationService.getSchema(dbConfigId);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/getTable")
+    public ResponseEntity<List<String>> getTableBySelectedSchema(@Param("dbConfigId") Long dbConfigId,@Param("schemaName") String schemaName) throws SQLException {
+        List<String> result = dbOperationService.getTablesBySelectedSchema(dbConfigId,schemaName);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/getColumns")
+    public ResponseEntity<List<Map<String,String>>> getColumnsAndDataType(@Param("dbConfigId") Long dbConfigId, @Param("schemaName") String schemaName, @Param("tableName") String tableName) throws SQLException {
+        List<Map<String,String>> result = dbOperationService.getColumnsAndTypeBySelectedTable(dbConfigId,schemaName,tableName);
+        return ResponseEntity.ok(result);
+    }
+
+}
