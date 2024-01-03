da05d16506188f17b0b6510ce99bae128f581ea7
Eren Eroğul <eren.eroul1903@gmail.com>
2024-01-03 11:48:56 +0300

    DM-22 task updated.

--- src/main/java/com/data/tools/api/constants/SQLConst.java
+++ src/main/java/com/data/tools/api/constants/SQLConst.java
@@ -13,9 +13,6 @@ public class SQLConst {
     public static String getTablesBySchema(String schemaName) {
         return String.format("SELECT table_name as Tables FROM all_tables WHERE owner = '%s'",schemaName);
     }
-   /* public static String getColumnsByTable(String schemaName, String tableName) {
-        return String.format("SELECT column_name,data_type FROM all_tab_columns WHERE owner = '%s' AND table_name = '%s'",schemaName,tableName);
-    }*/
 
     public static String getColumnsByTable(String schemaName, String tableName) {
         return String.format("SELECT atc.COLUMN_NAME, atc.DATA_TYPE, atc.DATA_LENGTH, atc.NULLABLE,( SELECT LISTAGG(ACC.CONSTRAINT_NAME, ', ') WITHIN GROUP (ORDER BY ACC.CONSTRAINT_NAME)FROM ALL_CONS_COLUMNS ACC WHERE ACC.TABLE_NAME = '%s' AND ACC.COLUMN_NAME = atc.COLUMN_NAME AND ACC.OWNER = '%s') AS CONSTRAINTS FROM ALL_TAB_COLUMNS atc WHERE atc.OWNER = '%s' AND atc.TABLE_NAME = '%s'",tableName,schemaName,schemaName,tableName);
