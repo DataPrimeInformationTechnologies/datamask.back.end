e784ca000f7f1452894ee7d44b534e91c209d3b3
aberatakdogan <akdoganberat@outlook.com>
2024-01-05 11:24:42 +0300

    Connection test service and controller classes are edited

--- src/main/java/com/data/tools/api/controller/DbConnectionController.java
+++ src/main/java/com/data/tools/api/controller/DbConnectionController.java
@@ -3,9 +3,7 @@ package com.data.tools.api.controller;
 import com.data.tools.api.entity.DBConfiguration2;
 import com.data.tools.api.service.DBConnectionService2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/connection")
@@ -14,14 +12,14 @@ public class DbConnectionController {
     @Autowired
     private DBConnectionService2 dbConnectionService;
 
-    @GetMapping("/test")
-    public String testConnection() {
-        DBConfiguration2 dbConfiguration = new DBConfiguration2();
-        dbConfiguration.setHost("34.125.48.246");
-        dbConfiguration.setPort("1521");
-        dbConfiguration.setServiceName("orcl");
-        dbConfiguration.setUsername("emre");
-        dbConfiguration.setPassword("ora123");
+    @PostMapping("/test")
+    public String testConnection(@RequestBody DBConfiguration2 dbConfiguration) {
+
+        //dbConfiguration.setHost("34.125.48.246");
+        //dbConfiguration.setPort("1521");
+        //dbConfiguration.setServiceName("orcl");
+        //dbConfiguration.setUsername("emre");
+        //dbConfiguration.setPassword("ora123");
 
         boolean result = dbConnectionService.testConnection(dbConfiguration);
 
