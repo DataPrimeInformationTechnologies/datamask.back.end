2791386593778052bd95fbe36338e53bc04f4385
aberatakdogan <akdoganberat@outlook.com>
2024-01-16 22:25:51 +0300

    Connection test service and controller classes are edited

--- src/main/java/com/data/tools/api/service/DBConnectionService2.java
+++ src/main/java/com/data/tools/api/service/DBConnectionService2.java
@@ -1,14 +1,15 @@
 package com.data.tools.api.service;
 
 import com.data.tools.api.entity.DBConfiguration2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Service;
-
 import javax.sql.DataSource;
 
 @Service
+@Slf4j
 public class DBConnectionService2 {
 
     @Autowired
@@ -22,6 +23,7 @@ public class DBConnectionService2 {
             return true;
         } catch (Exception e) {
             e.printStackTrace();
+            log.error(e.getMessage());
             return false;
         }
     }
