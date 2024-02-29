489e39f7b2560a53ebceb7665448de5ce721b0a1
Eren Eroğul <eren.eroul1903@gmail.com>
2024-02-29 15:40:35 +0300

    DM-26 conflict.

--- src/main/java/com/data/tools/api/constants/SQLConst.java
+++ src/main/java/com/data/tools/api/constants/SQLConst.java
@@ -2,19 +2,5 @@ package com.data.tools.api.constants;
 
 public class SQLConst {
 
-    public static String getSelectBySchemeAndTable(String schemaName, String tableName) {
-        return String.format("SELECT * FROM %s.%s", schemaName, tableName);
-    }
 
-    public static  String getSchemas() {
-        return "SELECT username as Schemas FROM all_users";
-    }
-
-    public static String getTablesBySchema(String schemaName) {
-        return String.format("SELECT table_name as Tables FROM all_tables WHERE owner = '%s'",schemaName);
-    }
-
-    public static String getColumnsByTable(String schemaName, String tableName) {
-        return String.format("SELECT atc.COLUMN_NAME, atc.DATA_TYPE, atc.DATA_LENGTH, atc.NULLABLE,( SELECT LISTAGG(ACC.CONSTRAINT_NAME, ', ') WITHIN GROUP (ORDER BY ACC.CONSTRAINT_NAME)FROM ALL_CONS_COLUMNS ACC WHERE ACC.TABLE_NAME = '%s' AND ACC.COLUMN_NAME = atc.COLUMN_NAME AND ACC.OWNER = '%s') AS CONSTRAINTS FROM ALL_TAB_COLUMNS atc WHERE atc.OWNER = '%s' AND atc.TABLE_NAME = '%s'",tableName,schemaName,schemaName,tableName);
-    }
 }
